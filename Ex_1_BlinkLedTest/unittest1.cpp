aa42a1855b82b7c3000638908a7c8c8d7bdf76ab
MinhHieuDL <minhhieuhcmute@gmail.com>
2019-01-09 13:31:50 +0700

    Create Project

---
+++ Ex_1_BlinkLedTest/unittest1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../Ex_1_BlinkLed/ExClass.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Ex_1_BlinkLedTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
