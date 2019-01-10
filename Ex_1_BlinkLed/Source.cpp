f13709214ea822167466f86660c5af4e29b194ee
MinhHieuDL <minhhieuhcmute@gmail.com>
2019-01-10 09:37:51 +0700

    Add Demo Project in file Source.cpp

--- Ex_1_BlinkLed/Source.cpp
+++ Ex_1_BlinkLed/Source.cpp
@@ -1,6 +1,32 @@
+#include <stdio.h>
 #include <iostream>
+#include "ExClass.h"
+#include <Windows.h>
+
+using namespace std;
+class DemoLed : public Iled
+{
+	virtual void TurnOn()
+	{
+		printf("Led is on\n");
+	}
+	virtual void TurnOff()
+	{
+		printf("Led is off\n");
+	}
+};
 
 int main(void)
 {
+	Blinker blinker;
+	DemoLed demoLed;
+	blinker.AttachLedHAL(&demoLed);
+
+	blinker.Execute(5, 10, 10);
+	while (true)
+	{
+		blinker.Tick1ms();
+		Sleep(100);
+	}
 	return 0;
 }
